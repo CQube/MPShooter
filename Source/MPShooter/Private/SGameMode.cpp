2564155f74b6770e6e071119e014b6d4c0d044b8
DmitryFrolov <dmfroloff@gmail.com>
2018-02-26 23:31:43 +0200

    player respawn

--- Source/MPShooter/Private/SGameMode.cpp
+++ Source/MPShooter/Private/SGameMode.cpp
@@ -42,6 +42,8 @@ void ASGameMode::PrepareToNextWave()
 	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
 
 	SetWaveState(EWaveState::WaitingToStart);
+
+	RespawnDeadPlayers();
 }
 
 
@@ -114,6 +116,18 @@ void ASGameMode::SetWaveState(EWaveState NewState)
 	if (ensureAlways(GS))
 	{
 		GS->SetWaveState(NewState);
+	}
+}
+
+void ASGameMode::RespawnDeadPlayers()
+{
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
+	{
+		APlayerController* PC = It->Get();
+		if (PC && PC->GetPawn() == nullptr)
+		{
+			RestartPlayer(PC);
+		}
 	}
 }
 
